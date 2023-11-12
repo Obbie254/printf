da1a979567dfc28c89d5bd334b8674fe7ee47721
Obbie254 <obbienjehia@gmail.com>
2023-11-12 21:27:01 +0300

    created the printf code that can handle 'c' and '%'.

---
+++ my_print.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * _printf - fxn that produces output according to a format
+ * @format: a character string
+ * @...: format specifier args
+ * Return: 1 on success
+ */
+int _printf(const char *format, ...)
+{
+	int num = 0;
+	va_list my_args;
+
+	va_start(my_args, format);
+	if (format == NULL)
+		return (-1);
+	while (*format != '\0')
+	{
+		if (*format == '%')
+		{
+			switch (*++format)
+			{
+				/* handle c */
+				case 'c':
+					char c = va_arg(my_args, int);
+
+					write(1, &c, 1);
+					num++;
+					break;
+				/* handle %% */
+				case '%':
+					write(1, format, 1);
+					num++;
+			}
+		}
+		else
+		{
+			write(1, format, 1);
+			num++;
+		}
+		format++
+	}
+	va_end(my_args);
+	return (num);
+}
