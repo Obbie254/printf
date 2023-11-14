37e702be534af5cd8118b858da24bb29ca917804
theekyrabrenda1 <theekyrabrenda@gmail.com>
2023-11-13 23:32:46 -0800

    Changes made on my_print.c file

--- my_print.c
+++ my_print.c
@@ -10,7 +10,7 @@
 int _printf(const char *format, ...)
 {
 	int num = 0, len;
-	char c, *s, v, p;
+	char c, *s;
 
 	va_list my_args;
 	va_start(my_args, format);
@@ -28,20 +28,16 @@ int _printf(const char *format, ...)
 					num++;
 					break;
 				case '%':
-					p = va_arg(my_args, int);
-					write(1, &p, 1);
+					write(1, format, 1);
 					num++;
 					break;
 				case 's':
 					s = va_arg(my_args, char*);
 					len = 0;
-					write(1, &s, str_len(s));
+					while (s[len] != '\0')
+						len++;
+					write(1, s, (len + 1));
 					num += len;
-					break;
-				default:
-					v = va_arg(my_args, int);
-					write(1, &v, 1);
-					num++;
 					break;
 			}
 		}
