7558cd44a9028cc893d701288007908a54a867d4
theekyrabrenda1 <theekyrabrenda@gmail.com>
2023-11-14 17:26:26 -0800

    Changes made to print_numbers.c file

--- print_numbers.c
+++ print_numbers.c
@@ -1,42 +1,49 @@
+/**
+ * _printf - function used
+ * @format: number to be printed according to the case
+ * @...: variadic function variables declaration
+ * Return: x
+ */
 #include <stdarg.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...)
 {
-    va_list nums;
-    int a, b, y, e, f, x;
-    a = va_arg(nums, int);
-    b = a % 10;
-    f = a / 10;
-    e = 1; x = 1;
-    
-    va_start(nums, format);
-    if (a < 0)
-    {
-        write(1, "-", 1);
-        f = -f;
-        a = -a;
-        b = -b;
-        x++;
-    }
-    if (f > 0)
-    {
-        while (f / 10 != 0)
-        {
-            e = e * 10;
-            f++;
-        }
-        f = a;
-        while (e > 0)
-        {
-            y = f / e;
-            write(1, (&y + '0'), 1);
-            f = f - (y * e);
-            e = e / 10;
-            x++;
-        }
-    }
-    write(1, (&b + '0'), 1);
-    va_end(nums);
-    return (x);
+va_list nums;
+int a, b, y, e, f, x;
+a = va_arg(nums, int);
+b = a % 10;
+f = a / 10;
+e = 1;
+x = 1;
+
+va_start(nums, format);
+if (a < 0)
+{
+write(1, "-", 1);
+f = -f;
+a = -a;
+b = -b;
+x++;
+}
+if (f > 0)
+{
+while (f / 10 != 0)
+{
+e = e * 10;
+f++;
+}
+f = a;
+while (e > 0)
+{
+y = f / e;
+write(1, (&y + '0'), 1);
+f = f - ((y)*(e));
+e = e / 10;
+x++;
+}
+}
+write(1, (&b + '0'), 1);
+va_end(nums);
+return (x);
 }
